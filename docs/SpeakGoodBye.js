5c09491195302f4b947e5b23e4d31be6cfa36a68
mame301186 <80558392+mame301186@users.noreply.github.com>
2021-06-04 17:20:59 -0400

    Copy the assignment 5 to folder docs for your deploy on github-pages

--- docs/SpeakGoodBye.js
+++
@@ -1,8 +0,0 @@
-(function(window) {
-    var speakWord = "Good Bye";
-    var byeSpeaker = {};
-    byeSpeaker.speak = function(names) {
-        console.log(speakWord + " " + names);
-    };
-    window.byeSpeaker = byeSpeaker;
-})(window);
--- docs/SpeakHello.js
+++
@@ -1,8 +0,0 @@
-(function(window) {
-    var speakWord = "Hello";
-    var helloSpeaker = {};
-    helloSpeaker.speak = function(names) {
-        console.log(speakWord + " " + names);
-    };
-    window.helloSpeaker = helloSpeaker
-})(window);
